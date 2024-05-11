c55090bb878c6748de7c373a6778fe96b1600fa9
Nathanael Mendes <nathan.mendes.ti@gmail.com>
2024-05-11 16:18:25 -0300

    feat: Add 304 and 500 HttpStatus

--- app/Enums/HttpStatus.php
+++ app/Enums/HttpStatus.php
@@ -6,6 +6,8 @@ enum HttpStatus: int
 {
     case OK = 200;
     case CREATED = 201;
+    case FOUND = 302;
     case NOT_FOUND = 404;
     case UNPROCESSABLE_CONTENT = 422;
+    case INTERNAL_ERROR = 500;
 }
