0ae7467ff43751ea41f6cd057a44d4fbe8f02564
Nathanael Mendes <nathan.mendes.ti@gmail.com>
2024-05-11 16:23:53 -0300

    feat: Bank\Account Integration Tests (/api/conta)

---
+++ tests/Feature/BankAccount/AccountTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace BankAccount;
+
+use App\Enums\HttpStatus;
+use Tests\TestCase;
+
+class AccountTest extends TestCase
+{
+    private string $baseUrl = '/api/conta';
+
+    public function testBankAccountExists(): void
+    {
+        $response = $this->get("$this->baseUrl?id=1");
+
+        $response->assertStatus(HttpStatus::OK->value);
+        $response->assertJsonStructure(['account_id', 'balance']);
+    }
+
+    public function testGetBankAccountMissingIdParameter(): void
+    {
+        $response = $this->get($this->baseUrl, ['Accept' => 'application/json']);
+
+        $response->assertStatus(HttpStatus::UNPROCESSABLE_CONTENT->value);
+        $response->assertJsonValidationErrors(['id']);
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'id'
+            ]
+        ]);
+    }
+
+    public function testBankAccountNotFound(): void
+    {
+        $response = $this->get("$this->baseUrl?id=9999");
+        $response->assertStatus(HttpStatus::NOT_FOUND->value);
+    }
+
+    public function testStoreBankAccount(): void
+    {
+        $response = $this->postJson($this->baseUrl, ['balance' => 500]);
+
+        $response->assertStatus(HttpStatus::CREATED->value);
+        $response->assertJsonStructure(['account_id', 'balance']);
+    }
+
+    public function testStoreBankAccountMissingBalanceParameter(): void
+    {
+        $response = $this->postJson($this->baseUrl, [], ['Accept' => 'application/json']);
+
+        $response->assertStatus(HttpStatus::UNPROCESSABLE_CONTENT->value);
+        $response->assertJsonValidationErrors(['balance']);
+        $response->assertJsonStructure([
+            'message',
+            'errors' => [
+                'balance'
+            ]
+        ]);
+    }
+}
